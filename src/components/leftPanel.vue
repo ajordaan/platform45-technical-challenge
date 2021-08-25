e10bd650113c2a89b9677121045fb3266bd12c04
Andrew Jordaan <ajordaan23@gmail.com>
2021-08-25 15:36:39 +0200

    Added left panel

---
+++ src/components/leftPanel.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="leftPanel">
+    <img class="avatar" :src="require(`@/assets/images/face.svg`)" />
+
+    <div>
+      <p class="title">Front-end challenge!</p>
+      <p class="subtitle mt-3">
+        This is a design that you will need <br />
+        to code up and impress us
+      </p>
+    </div>
+    <div class="arrowContainer">
+      <img class="arrow" :src="require(`@/assets/images/arrow.svg`)" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.leftPanel {
+  background-color: yellow;
+}
+
+.arrowContainer {
+  width: 65px;
+  height: 65px;
+  background: black;
+  margin: 0 auto;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10%;
+  margin-bottom: 15%;
+}
+.avatar {
+  max-height: 80%;
+  padding-top: 20%;
+  padding-bottom: 15%;
+}
+</style>
