36e70969861cc242afe1ac2ac84af6e13053e6f5
Andrew Jordaan <ajordaan23@gmail.com>
2021-08-25 16:56:10 +0200

    Added form icon component

---
+++ src/components/formIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="iconContainer">
+    <img :src="require('@/assets/images/' + this.imageUrl)" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imageUrl: { type: String, required: true },
+  },
+};
+</script>
+
+<style></style>
